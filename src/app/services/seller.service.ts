06343cfc95ce9a10ba24bec1a38a9aeb8482b6e5
mehedi <msmbd504@gmail.com>
2023-01-28 22:07:28 +0600

    remove unnecessary code

--- src/app/services/seller.service.ts
+++ src/app/services/seller.service.ts
@@ -19,7 +19,6 @@ export class SellerService {
       if (result) {
         localStorage.setItem("seller", JSON.stringify(result.body))
         this.router.navigate(['seller-home']);
-        console.log(result);
       }
 
     });
@@ -30,7 +29,6 @@ export class SellerService {
       if (result && result.body && result.body.length) {
         localStorage.setItem("seller", JSON.stringify(result.body))
         this.router.navigate(['seller-home']);
-        console.log(result);
       } else {
         this.isLoginError.emit(true);
       }
