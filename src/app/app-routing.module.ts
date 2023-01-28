d55d2dd49c3e4cf1f34141a6a97b5e7eeda2dfd8
mehedi <msmbd504@gmail.com>
2023-01-28 22:10:16 +0600

    add search, productDetails and userAuth routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import {SellerHomeComponent} from './seller-home/seller-home.component';
 import {SellerAddProductComponent} from "./seller-add-product/seller-add-product.component";
 import {SellerUpdateProductComponent} from "./seller-update-product/seller-update-product.component";
 import {SearchComponent} from "./search/search.component";
+import {ProductDetailsComponent} from "./product-details/product-details.component";
+import {UserAuthComponent} from "./user-auth/user-auth.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -14,7 +16,9 @@ const routes: Routes = [
   {path: 'seller-home', component: SellerHomeComponent, canActivate: [AuthGuard]},
   {path: 'seller-add-product', component: SellerAddProductComponent, canActivate: [AuthGuard]},
   {path: 'seller-update-product/:id', component: SellerUpdateProductComponent, canActivate: [AuthGuard]},
-  {path: 'search/:query', component: SearchComponent}
+  {path: 'search/:query', component: SearchComponent},
+  {path: 'details/:productId', component: ProductDetailsComponent},
+  {path: 'user-auth', component: UserAuthComponent}
 ]
 ;
 
