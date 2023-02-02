ffa2b9b5756516d368515b474b8544583b6e30cb
mehedi <msmbd504@gmail.com>
2023-02-02 10:49:12 +0600

    Eslint issue fixed

--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {userSignIn, userSignUp} from "../type/userAuthInfo";
 import {UserService} from "../services/user.service";
+import {cartItem, ProductData} from "../type/productData";
+import {ProductService} from "../services/product.service";
 
 @Component({
   selector: 'app-user-auth',
@@ -8,11 +10,11 @@ import {UserService} from "../services/user.service";
   styleUrls: ['./user-auth.component.css']
 })
 export class UserAuthComponent implements OnInit{
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private productService: ProductService) {
   }
 
-  isShowForm: boolean = true;
-  isAuthError: string = '';
+  isShowForm = true;
+  isAuthError = '';
 
   signUp(signUpData: userSignUp): void {
     this.userService.userSignUp(signUpData);
@@ -34,6 +36,22 @@ export class UserAuthComponent implements OnInit{
 
   openSignUp() {
     this.isShowForm = false;
+  }
+  localCartToRemoteCart(){
+    const data = localStorage.getItem('cartData');
+    if (data) {
+      const cartDataList: ProductData[] = JSON.parse(data);
+      const user = localStorage.getItem('user');
+      const userId = user && JSON.parse(user).id;
+      cartDataList.forEach((product: ProductData)=> {
+        const cartData : cartItem = {
+          ...product,
+          productId: product.id,
+          userId
+        };
+        this.productService.addToCart(cartData);
+      })
+    }
   }
 
   ngOnInit(): void {
