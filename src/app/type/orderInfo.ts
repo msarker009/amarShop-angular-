243176c2062e174e4ffd2cbe3fc1f4eec9a752cf
mehedi <msmbd504@gmail.com>
2023-02-13 02:30:22 +0600

    added orderInformation type

---
+++ src/app/type/orderInfo.ts
@@ -0,0 +1,14 @@
+export interface orderInfo {
+  email: string;
+  address: string;
+  contact: string;
+}
+export interface orderDetails {
+  email: string;
+  address: string;
+  contact: string;
+  totalPrice: number;
+  userId: number;
+  id: number | undefined;
+
+}
